6af6a11ef365b74967c466d33a102eb9c69c051e
wongsing <277245106@qq.com>
2020-11-12 00:03:16 +0800

    page9 from wxy!!!

--- SLM/pages/page9/page9.js
+++ SLM/pages/page9/page9.js
@@ -5,9 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    baseData1: [
+      {
+        name: '广科-松山湖校区',
+        ftNum:2,
+        foodNum:80,
+        id: 1
+      },
+      // {
+      //   name: '广科-南城校区',
+      //   ftNum:3,
+      //   foodNum:70,
+      //   id: 2
+      // }
+    ],
+    baseData2: [
+      {
+        name: '麻辣香锅',
+        rlNum:300,
+        id: 1
+      },
+      {
+        name: '鸡扒饭',
+        rlNum:200,
+        id: 2
+      },
+      // {
+      //   name: '滑蛋饭',
+      //   rlNum:250,
+      //   id: 3
+      // },
+    ],
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
